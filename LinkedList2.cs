8c84605c69da840607030e9e172e9dffa9e7b91b
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 10:56:55 -0800

    adding Program file

---
+++ LinkedList2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListSolu
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+
+                temp.Next = node;
+            }
+
+            Console.WriteLine("{0} inserted into linked list ", node.data);
+        }
+
+
+        public void Display()
+        {
+            Node tmp = this.head;
+
+            if (tmp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (tmp != null)
+            {
+                Console.WriteLine(tmp.data + " ");
+                tmp = tmp.Next;
+            }
+        }
+
+        internal void Peek()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Stack is empty");
+
+                return;
+            }
+        }
+
+
+        internal void pop()
+        {
+            if(this.head==null)
+            {
+                Console.WriteLine("Deletion is not possible");
+            }
+
+            Console.WriteLine("Vlaue popped" + this.head.data);
+            this.head = this.head.Next;
+        }
+
+        internal void IsEmp()
+        {
+            while(this.head!=null)
+            {
+                Peek();
+                pop();
+            }
+        }
+    }
+}
+
+
+
+
+
