cf6f772de03b7df2337037e13faaf7e08e621289
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 11:03:25 -0800

    adding pro file

---
+++ LinkedList4.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUC3
+{
+    public class LinkedQueue
+    {
+        Node head = null;
+
+
+
+
+
+
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+
+                temp.Next = node;
+            }
+
+            Console.WriteLine("{0} inserted into linked list ", node.data);
+        }
+
+        public void Display()
+        {
+            Node tmp = this.head;
+
+            if (tmp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (tmp != null)
+            {
+                Console.WriteLine(tmp.data + " ");
+                tmp = tmp.Next;
+            }
+        }
+
+
+        internal void dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Deletion is not possible");
+            }
+
+            Console.WriteLine("Vlaue Dequeue" + this.head.data);
+            this.head = this.head.Next;
+        }
+    }
+
+
+
+}
