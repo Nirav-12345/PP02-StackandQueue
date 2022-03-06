933bdd762215b24519f748d8b56cab67b1ce0dcc
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 10:55:17 -0800

    adding lass file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LinkedListSolu
+{
+    class rogram
+    {
+        static void Main(string[] args)
+        {
+            //int Choice,data,p;
+
+            Console.WriteLine("Welcome to Linked List operation");
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.pop();
+            list.Peek();
+            list.IsEmp();
+
+
+
+        }
+    }
+}
