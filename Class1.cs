9e8ab1d62dcf25c3a119bc7accbaceb70af4e6e2
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 10:48:22 -0800

    adding class file

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinkedListSolu
+{
+    class rogram
+    {
+        static void Main(string[] args)
+        {
+            //int Choice,data,p;
+
+            Console.WriteLine("Welcome to Linked List operation");
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+
+
+        }
+    }
+}
